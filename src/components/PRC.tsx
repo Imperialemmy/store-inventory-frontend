31571d3227f886c0f69e3ab1ab02967f8d0f5f2b
oluwaseun <oluwaseunadeeko124@gmail.com>
2025-07-31 21:42:04 +0100

    feat: implement PrivateRoute component to protect routes based on JWT token presence

---
+++ src/components/PRC.tsx
@@ -0,0 +1,10 @@
+import { JSX } from "react";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  const token = localStorage.getItem("access_token");
+
+  return token ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
