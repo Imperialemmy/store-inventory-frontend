cd292cd796f65fede79ceaf960fa731f45a87078
oluwaseun <oluwaseunadeeko124@gmail.com>
2025-07-31 20:45:05 +0100

    feat: render DashboardMenu on home page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import DashboardMenu from '../../features/dashboard/DashboardMenu';
+
+const Home = () => {
+  return (
+    <div className="p-6">
+      <DashboardMenu />
+    </div>
+  );
+};
+
+export default Home;
