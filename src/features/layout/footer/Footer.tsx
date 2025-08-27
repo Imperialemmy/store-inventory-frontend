5c2e2f7b5ce8d1ebff663e7a730254b06df6bfed
oluwaseun <oluwaseunadeeko124@gmail.com>
2025-08-27 09:53:51 +0100

    Changed rendering style

--- src/features/layout/footer/Footer.tsx
+++ src/features/layout/footer/Footer.tsx
@@ -1,12 +1,8 @@
-import React from "react";
-
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-white-900 text-black text-center py-4 fixed bottom-0 w-full">
-        <div className="border-t border-gray-400 mx-8"></div>
+    <footer className="bg-white text-black text-center py-4 border-t border-gray-400 w-full">
       <p className="mt-2">© 2025 Akinfolu Foods. All rights reserved.</p>
     </footer>
   );
 };
-
 export default Footer;
