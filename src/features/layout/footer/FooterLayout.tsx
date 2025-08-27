3073c1c7b5cd21df732699f1b28ba3c08d29a244
oluwaseun <oluwaseunadeeko124@gmail.com>
2025-08-27 09:52:27 +0100

    refactor: prepare layout for future feature

---
+++ src/features/layout/footer/FooterLayout.tsx
@@ -0,0 +1,10 @@
+import Footer from "./Footer";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
